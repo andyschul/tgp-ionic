caa9c52e04bb7a7cccfe3899a1dc9ce27c2336cd
Andrew Schultz <abschultz1@gmail.com>
2019-11-23 19:51:11 -0500

    Get token from amplify

--- src/index.js
+++ src/index.js
@@ -3,32 +3,32 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import { ApolloProvider } from '@apollo/react-hooks'
-import { ApolloClient } from 'apollo-client';
-import { ApolloLink } from 'apollo-client-preset'
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import {
+  ApolloClient,
+  InMemoryCache,
+  from,
+} from "apollo-client-preset";
 import { HttpLink } from 'apollo-link-http';
+import { setContext } from "apollo-link-context";
+import { Auth } from 'aws-amplify';
 
 const httpLink = new HttpLink({
-    uri: 'http://localhost:4000/graphql'
-  })
-  
-  const middlewareAuthLink = new ApolloLink((operation, forward) => {
-    const lastAuthUser = localStorage.getItem('CognitoIdentityServiceProvider.4cmfst1kti8is4t1v2nm2f7in8.LastAuthUser');
-    const token = localStorage.getItem(`CognitoIdentityServiceProvider.4cmfst1kti8is4t1v2nm2f7in8.${lastAuthUser}.idToken`)
-    operation.setContext({
-      headers: {
-        Authorization: token
-      }
-    })
-    return forward(operation)
-  })
-  
-  const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
-  
-  const client = new ApolloClient({
-    link: httpLinkWithAuthToken,
-    cache: new InMemoryCache()
-  });
+  uri: 'http://localhost:4000/graphql'
+})
+
+const authMiddleware = setContext(async (req, { headers }) => {
+  const session = await Auth.currentSession();
+  return {
+    headers: {
+      Authorization: session.idToken.jwtToken
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: from([authMiddleware, httpLink]),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
 <ApolloProvider client={client}>
