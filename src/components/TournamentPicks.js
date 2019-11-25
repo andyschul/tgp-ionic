aff5605f775fef674ee9cf8de432508f63800746
Andrew Schultz <abschultz1@gmail.com>
2019-11-25 13:17:02 -0500

    Make selections

--- src/components/TournamentPicks.js
+++ src/components/TournamentPicks.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import { IonContent, IonPage, IonButton } from '@ionic/react';
-import { useQuery } from '@apollo/react-hooks';
+import React, {useState} from 'react';
+import { IonContent, IonToast, IonPage, IonList, IonRadioGroup, IonListHeader, IonLabel, IonRadio, IonItem } from '@ionic/react';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 import AppHeader from './AppHeader'
-import PlayerList from './PlayerList'
 
 const GET_TOURNAMENT = gql`
   query GetTournamentPicks($id: ID!, $groupId: ID) {
@@ -24,30 +23,80 @@ const GET_TOURNAMENT = gql`
   }
 `;
 
+const UPDATE_PICKS = gql`
+  mutation updatePicks($groupId: ID!, $tournamentId: ID!, $picks: [String]) {
+    updatePicks(groupId: $groupId, tournamentId: $tournamentId, picks: $picks) {
+      success
+    }
+  }
+`;
+
 export default function TournamentPicks(route) {
-    const { data, loading, error } = useQuery(
+    const { data, loading, error, updateQuery } = useQuery(
         GET_TOURNAMENT,
         { variables: { id: route.match.params.tournamentId, groupId: route.match.params.groupId } }
     );
+    const [updatePicks] = useMutation(UPDATE_PICKS);
+    const [showToast, setShowToast] = useState(false);
     if (loading) return '';
     if (error) return `Error! ${error.message}`;
 
-    function handleSubmit(e, d) {
-        e.preventDefault();
-        console.log(e.target, d)
+    function handleSubmit() {
+        let picks = []
+        for (let g of data.tournament.groups) {
+            for (let p of g.players) {
+                if (p.isSelected) {
+                    picks.push(p.id);
+                    break;
+                }
+            }
+        }
+        updatePicks({ variables: { groupId: route.match.params.groupId, tournamentId: route.match.params.tournamentId, picks: picks } });
+    }
+
+    function handleSelect(idx, playerId) {
+        updateQuery(x=>{
+            x.tournament.groups[idx].players = x.tournament.groups[idx].players.map(p=>({...p, isSelected: p.id === playerId}))
+        })
+        setShowToast(true)
       }
 
     return (
         <IonPage>
             <AppHeader />
             <IonContent className="ion-padding">
+            <IonToast
+                isOpen={showToast}
+                onDidDismiss={() => setShowToast(false)}
+                position="bottom"
+                buttons={[
+                {
+                    side: 'start',
+                    text: 'Cancel',
+                    role: 'cancel'
+                },
+                {
+                    text: 'Save',
+                    handler: () => {
+                        handleSubmit();
+                    }
+                }
+                ]}
+            />
                 <div>Tournament {data.tournament.name}</div>
-                <form onSubmit={handleSubmit}>
-                <IonButton type="submit">Save</IonButton>
                 {data.tournament.groups.map((g, idx) => 
-                    <PlayerList key={g.id} idx={idx} groupId={g.id} players={g.players}></PlayerList>
+                    <IonList key={g.id}>
+                        <IonRadioGroup>
+                        <IonListHeader>Group {g.id}</IonListHeader>
+                            {g.players.map(p => 
+                            <IonItem key={p.id}>
+                                <IonLabel>{`${p.firstName} ${p.lastName} (${p.country})`}</IonLabel>
+                                <IonRadio value={p.id} checked={p.isSelected} onIonSelect={()=>handleSelect(idx, p.id)} />
+                            </IonItem>
+                            )}
+                        </IonRadioGroup>
+                    </IonList>
                 )}
-                </form>
             </IonContent>
         </IonPage>
     )
